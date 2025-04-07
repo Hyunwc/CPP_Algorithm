e2acd4abb6033a089a9fe0f7906b29b521eebb12
Hyunwc <107088369+Hyunwc@users.noreply.github.com>
2025-04-07 16:40:37 +0900

    Add_RedBlackTree_Insert

--- Algorithm/Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm/Algorithm.cpp
@@ -3,6 +3,7 @@
 #include <list>
 #include <stack>
 #include <queue>
+#include <thread>
 using namespace std;
 #include "BinarySearchTree.h"
 
@@ -12,18 +13,29 @@ int main()
 {
 	BinarySearchTree bst;
 
-	bst.Insert(20);
-	bst.Insert(10);
 	bst.Insert(30);
-	bst.Insert(25);
-	bst.Insert(26);
-	bst.Insert(40);
-	bst.Insert(50);
-
-	bst.Delete(20);
-	bst.Delete(26);
-
 	bst.Print();
+	this_thread::sleep_for(1s);
+
+	bst.Insert(10);
+	bst.Print();
+	this_thread::sleep_for(1s);
+
+	bst.Insert(20);
+	bst.Print();
+	this_thread::sleep_for(1s);
+
+	bst.Insert(25);
+	bst.Print();
+	this_thread::sleep_for(1s);
+
+	bst.Insert(40);
+	bst.Print();
+	this_thread::sleep_for(1s);
+
+	bst.Insert(50);
+	bst.Print();
+	this_thread::sleep_for(1s);
 }
 
 
